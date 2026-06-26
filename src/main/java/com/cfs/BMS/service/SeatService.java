6bde3847ceda0b98c02766c2ac622f1fd1bf64da
Sanjeev Maurya <163615408+SanjeevMaurya63@users.noreply.github.com>
2026-06-26 23:53:34 +0530

    Update SeatService.java

--- src/main/java/com/cfs/BMS/service/SeatService.java
+++ src/main/java/com/cfs/BMS/service/SeatService.java
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 
+@Service
+@RequiredArgsConstructor
 public class SeatService {
 
     private final SeatRepository seatRepository;
